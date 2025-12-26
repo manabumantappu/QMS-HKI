27a22c939de76d4185e4e64bcc0f9074e6fcaf04
manabumantappu <azishachigo@gmail.com>
2025-12-26 15:49:21 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,20 @@
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  // USER DEMO (BISA DIUBAH)
+  if (user === "operator" && pass === "123") {
+    setRole("operator");
+  } else if (user === "qc" && pass === "123") {
+    setRole("qc");
+  } else if (user === "auditor" && pass === "123") {
+    setRole("auditor");
+  } else {
+    alert("Username / Password salah");
+  }
+}
+
+function setRole(role) {
+  localStorage.setItem("qms_role", role);
+  window.location.href = "dokumen.html";
+}
